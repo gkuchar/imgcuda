1b74f56b7db5967d6929f51a8b86ebd570971df2
Griffin Kuchar <griffin.kuchar@gmail.com>
2026-02-22 15:04:54 -0600

    implemented parallel and sequential grayscale functions

--- src/imgcuda.cu
+++ src/imgcuda.cu
@@ -39,6 +39,42 @@ __global__ void drop_blue_kernel(unsigned char* data, int width, int height) {
     }
 }
 
+__global__ void grayscale_kernel(unsigned char* data, int width, int height) {
+    int x = threadIdx.x + blockIdx.x * blockDim.x;
+    int y = threadIdx.y + blockIdx.y * blockDim.y;
+
+    if (x < width && y < height) {
+        int offset = y * width + x;
+
+        unsigned char r = data[CHANNELS * offset]; // red value for pixel
+        unsigned char g = data[CHANNELS * offset + 1]; // green value for pixel
+        unsigned char b = data[CHANNELS * offset + 2]; // blue value for pixel
+
+        float gray_val = 0.21f*r + 0.71f*g + 0.07f*b;
+        data[CHANNELS * offset] = (unsigned char)gray_val;
+        data[CHANNELS * offset + 1] = (unsigned char)gray_val;
+        data[CHANNELS * offset + 2] = (unsigned char)gray_val;
+    }
+}
+
+__global__ void blur_kernel(unsigned char* data, int width, int height, int radius) {
+    int x = threadIdx.x + blockIdx.x * blockDim.x;
+    int y = threadIdx.y + blockIdx.y * blockDim.y;
+
+    if (x < width && y < height) {
+        int offset = y * width + x;
+
+        unsigned char r = data[CHANNELS * offset]; // red value for pixel
+        unsigned char g = data[CHANNELS * offset + 1]; // green value for pixel
+        unsigned char b = data[CHANNELS * offset + 2]; // blue value for pixel
+
+        float gray_val = 0.21f*r + 0.71f*g + 0.07f*b;
+        data[CHANNELS * offset] = (unsigned char)gray_val;
+        data[CHANNELS * offset + 1] = (unsigned char)gray_val;
+        data[CHANNELS * offset + 2] = (unsigned char)gray_val;
+    }
+}
+
 namespace imgcuda {
     ppm::Image drop_red(const ppm::Image& in, imgcuda::Timing& t) {
         cudaEvent_t start, stop;
@@ -208,8 +244,118 @@ namespace imgcuda {
         return drop_blue(in, t);
     }
 
-    ppm::Image grayscale(const ppm::Image& in, Timing& t) { return in; } // TODO
-    ppm::Image blur(const ppm::Image& in, int /*radius*/, Timing& t) { return in; } // TODO
+    ppm::Image grayscale(const ppm::Image& in, imgcuda::Timing& t) {
+        cudaEvent_t start, stop;
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+        float ms;
+
+        // 1. Allocate device memory
+        unsigned char* d_data;
+        cudaError_t err = cudaMalloc(&d_data, in.bytes());
+        if (err != cudaSuccess) printf("CUDA error: %s\n", cudaGetErrorString(err));
+
+        // 2. Copy input to device
+        cudaEventRecord(start);
+        err = cudaMemcpy(d_data, in.data.data(), in.bytes(), cudaMemcpyHostToDevice);
+        if (err != cudaSuccess) printf("CUDA error: %s\n", cudaGetErrorString(err));
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&ms, start, stop);
+        t.htd += ms;
+
+        // 3. Launch kernel
+        dim3 block(16, 16);
+        dim3 grid((in.width + block.x - 1) / block.x, (in.height + block.y - 1) / block.y);
+        cudaEventRecord(start);
+        grayscale_kernel<<<grid, block>>>(d_data, in.width, in.height);
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&ms, start, stop);
+        t.kernel += ms;
+
+        // 4. Copy result back
+        ppm::Image out = in; // copy metadata (width, height)
+        cudaEventRecord(start);
+        err = cudaMemcpy(out.data.data(), d_data, out.bytes(), cudaMemcpyDeviceToHost);
+        if (err != cudaSuccess) printf("CUDA error: %s\n", cudaGetErrorString(err));
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&ms, start, stop);
+        t.dth += ms;
+
+        // 5. Free device memory
+        err = cudaFree(d_data);
+        if (err != cudaSuccess) printf("CUDA error: %s\n", cudaGetErrorString(err));
+
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+
+        return out;
+    }
+
+    // original API wraps timing version and discards metrics
+    ppm::Image grayscale(const ppm::Image& in) {
+        imgcuda::Timing t;
+        return grayscale(in, t);
+    }
+
+    ppm::Image blur(const ppm::Image& in, int radius, imgcuda::Timing& t) {
+        cudaEvent_t start, stop;
+        cudaEventCreate(&start);
+        cudaEventCreate(&stop);
+        float ms;
+
+        // 1. Allocate device memory
+        unsigned char* d_data;
+        cudaError_t err = cudaMalloc(&d_data, in.bytes());
+        if (err != cudaSuccess) printf("CUDA error: %s\n", cudaGetErrorString(err));
+
+        // 2. Copy input to device
+        cudaEventRecord(start);
+        err = cudaMemcpy(d_data, in.data.data(), in.bytes(), cudaMemcpyHostToDevice);
+        if (err != cudaSuccess) printf("CUDA error: %s\n", cudaGetErrorString(err));
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&ms, start, stop);
+        t.htd += ms;
+
+        // 3. Launch kernel
+        dim3 block(16, 16);
+        dim3 grid((in.width + block.x - 1) / block.x, (in.height + block.y - 1) / block.y);
+        cudaEventRecord(start);
+        blur_kernel<<<grid, block>>>(d_data, in.width, in.height, radius);
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&ms, start, stop);
+        t.kernel += ms;
+
+        // 4. Copy result back
+        ppm::Image out = in; // copy metadata (width, height)
+        cudaEventRecord(start);
+        err = cudaMemcpy(out.data.data(), d_data, out.bytes(), cudaMemcpyDeviceToHost);
+        if (err != cudaSuccess) printf("CUDA error: %s\n", cudaGetErrorString(err));
+        cudaEventRecord(stop);
+        cudaEventSynchronize(stop);
+        cudaEventElapsedTime(&ms, start, stop);
+        t.dth += ms;
+
+        // 5. Free device memory
+        err = cudaFree(d_data);
+        if (err != cudaSuccess) printf("CUDA error: %s\n", cudaGetErrorString(err));
+
+        cudaEventDestroy(start);
+        cudaEventDestroy(stop);
+
+        return out;
+    }
+
+    // original API wraps timing version and discards metrics
+    ppm::Image blur(const ppm::Image& in, int radius) {
+        imgcuda::Timing t;
+        return grayscale(in, radius, t);
+    }
+
     ppm::Image sharpen(const ppm::Image& in, Timing& t)   { return in; } // TODO
     ppm::Image sobel_x(const ppm::Image& in, Timing& t)   { return in; } // TODO
     ppm::Image sobel_y(const ppm::Image& in, Timing& t)   { return in; } // TODO
--- src/imgreference.cpp
+++ src/imgreference.cpp
@@ -33,7 +33,25 @@ namespace imgcpu {
         return out;
     }
 
-    ppm::Image grayscale(const ppm::Image& in) { return in; } // TODO
+    ppm::Image grayscale(const ppm::Image& in) {
+        int i = 0;
+        ppm::Image out = in;
+        while (i < (int)in.bytes()) {
+            unsigned char r = in.data[i];
+            unsigned char g = in.data[i + 1];
+            unsigned char b = in.data[i + 2];
+
+            float gray_val = 0.21f*r + 0.71f*g + 0.07f*b;
+
+            out.data[i] = (unsigned char)gray_val;
+            out.data[i + 1] = (unsigned char)gray_val;
+            out.data[i + 2] = (unsigned char)gray_val;
+
+            i = i + 3;
+        }
+        return out;
+    }
+
     ppm::Image blur(const ppm::Image& in, int /*radius*/) { return in; } // TODO
     ppm::Image sharpen(const ppm::Image& in)   { return in; } // TODO
     ppm::Image sobel_x(const ppm::Image& in)   { return in; } // TODO
